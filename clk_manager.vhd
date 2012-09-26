977b6b84d0b906f1f9d3a319779fba86f4680400
Daniel <chef.the.dza@gmail.com>
2012-09-26 11:08:43 -0700

    Update clk_manager.vhd
    Updated my clk_manager process to include a 4:1 MUX.

--- clk_manager.vhd
+++ clk_manager.vhd
@@ -1,7 +1,12 @@
 ----------------------------------------------------------------------------------
 -- Daniel Mazo
 -- Create Date:    10:41:56 09/24/2012 
--- Module Name:    clk_manager - Behavioral 
+-- Module Name:    clk_manager - Behavioral
+
+-- Description:
+-- This VHDL process will take a CLK_MAX as an input and then divide it down
+-- into 3 "smaller" clock signals. Then a 4:1 MUX will select the appropriate
+-- "slower clock" to send off to other processes.
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -16,15 +21,15 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 entity clk_manager is
 	Port(	clk_max	:	in	STD_LOGIC;
 			rst		:	in STD_LOGIC;
-			clk_2		:	out STD_LOGIC;
-			clk_4		:	out STD_LOGIC;
-			clk_8		: 	out STD_LOGIC);
+			sel		:	in STD_LOGIC_VECTOR(1 downto 0);
+			clk_out	:	out STD_LOGIC);
 end clk_manager;
 
 architecture Behavioral of clk_manager is
-type statetype is (s0,s1,s2,s3,s4,s5);
+-- SIGNALS
+type statetype is (s0,s1,s2,s3,s4,s5,s6);
 signal state, nextstate : statetype;
---signal clk_bar : STD_LOGIC;
+signal clk_2,clk_4,clk_6 : STD_LOGIC;
 
 begin
 	-- state register
@@ -32,7 +37,8 @@ begin
 	begin
 		if rst = '1' then state <= s0;
 		elsif clk_max'event and clk_max = '1' then state <= nextstate;
-		end if;
+		end if;	
+		
 	end process;
 	
 	-- next state logic
@@ -41,16 +47,20 @@ begin
 					 s3 when state = s2 else
 					 s4 when state = s3 else
 					 s5 when state = s4 else
+					 s6 when state = s5 else
 					 s0;
 					 
 	-- output logic
-	-- !!! Think about changing to CASE STATEMENT !!!
-	clk_2 <= '1' when state = s1 else '0';
-	clk_4 <= '1' when state = s3 else '0';
-	clk_8 <= '1' when state = s5 else '0';
+	clk_2 <= '1' when state = s2 else '0';
+	clk_4 <= '1' when state = s4 else '0';
+	clk_6 <= '1' when state = s6 else '0';
 
-
-	-- MUX Process?
+	-- MUX Process
+	with sel select clk_out <=
+		clk_2 when "00",
+		clk_4 when "01",
+		clk_6 when "10",
+		'0' when others;
 
 end Behavioral;
 
